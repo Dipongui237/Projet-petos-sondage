511b780cc81cf6a7588f485a83af02dd025f61d2
Dipongui237 <berthe.elongui@gmail.com>
2025-06-22 05:53:03 +0200

    modification du viteconfig.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Projet-petos-sondage/',
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources/js'),
+    },
+  },
 })
